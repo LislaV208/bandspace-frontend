67b86c0069e181f84f9a78040e96c787bc93122c
Sebastian Lisiecki <lislav.hms@gmail.com>
2024-12-12 21:11:41 +0100

    logowanie

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,11 @@ import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
 export default withMiddlewareAuthRequired();
 
 export const config = {
-  matcher: ['/dashboard/:path*']
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api/auth (Auth0 authentication endpoints)
+     */
+    '/((?!api/auth).*)',
+  ]
 };
